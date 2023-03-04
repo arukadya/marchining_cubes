707b68db10fdc72e3f48f952da12fe961cafdc9c
arukadya <a19.sj7x@g.chuo-u.ac.jp>
2023-03-04 12:46:43 +0900

    Initial Commit

---
+++ marchining_cubes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  marchining_cubes
+//
+//  Created by 須之内俊樹 on 2023/03/04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
